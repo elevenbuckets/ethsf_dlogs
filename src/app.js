33fc951736af278634173074e24404920ade4152
liang <zh_l2006@sina.com>
2018-10-06 20:30:15 -0600

    add ipfs and dlogs

--- src/app.js
+++ src/app.js
@@ -2,16 +2,4 @@ import MainView from "./view/MainView";
 import React from "react";
 import ReactDom from "react-dom";
 
-const remote = require('electron').remote;
-const ipfs = remote.getGlobal('ipfs');
-const DLogsAPI = require('../DLogsAPI.js');
-
-const dlogs = new DLogsAPI('../.local/config.json');
-
-ipfs.ipfsAPI.id()
-  .then((o) => { console.log(JSON.stringify(o,0,2)) })
-  .then(() => { return dlogs.connect() })
-  .then(() => { return dlogs.init(ipfs) })
-  .then((r) => { if (r) console.log(dlogs.web3.eth.blockNumber) })
-
 ReactDom.render(<MainView />, document.getElementById("app"))
--- src/store/DlogsStore.js
+++ src/store/DlogsStore.js
@@ -4,7 +4,18 @@ import DlogsActions from "../action/DlogsActions";
 class DlogsStore extends Reflux.Store {
     constructor() {
         super();
-        this.listenables = DlogsActions
+        this.listenables = DlogsActions;
+        const remote = require('electron').remote;
+        this.ipfs = remote.getGlobal('ipfs');
+        const DLogsAPI = require('../../DLogsAPI.js');
+
+        this.dlogs = new DLogsAPI('../.local/config.json');
+
+        this.ipfs.ipfsAPI.id()
+            .then((o) => { console.log(JSON.stringify(o, 0, 2)) })
+            .then(() => { return dlogs.connect() })
+            .then(() => { return dlogs.init(ipfs) })
+            .then((r) => { if (r) console.log(dlogs.web3.eth.blockNumber) })
 
         this.state = {
             blogs: [{
@@ -19,12 +30,33 @@ class DlogsStore extends Reflux.Store {
                 timeStamp: "123123",
                 content: "# This is Blog 1\n" + "This is the content 1"
             }
-            ]
+            ],
+            following : [],
+            displayBlogs:[],
+            onlyShowForBlogger : "0x89dc07a2f750cf62e757f909a4985a94a07d6359",
+
         }
+        this.initializeState();
+    }
+
+    initializeState = () =>{
+        let ipns = this.dlogs.lookUpByAddr(this.state.onlyShowForBlogger);
+        this.ipfs.resolve(ipns).then(r =>{return this.ipfs.readPath(r)})
+        .then(r=>{
+            let metaJSON = JSON.parse(r.toString());
+            let blogs = Object.keys(metaJSON.Articles).map(hash =>{
+                return {...metaJSON.Articles[hash], ipfsHash : hash}
+            })
+            this.setState({blogs: blogs})
+        })
+    }
+
+    refreshBlogs = () =>{
+        
     }
 
     onSaveNewBlog = (title, content) => {
-        let blog = {title, content}
+        let blog = { title, content }
         this.setState({ blogs: [...this.state.blogs, blog] })
     }
 

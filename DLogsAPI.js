25f993ad60cd3d728583c87f7b9f6da0e1f93ba3
jasonlin <jasonlin@elevenbuckets.com>
2018-10-15 16:35:21 -0600

    first PoC, untested DLogs using BladeIron

--- DLogsAPI.js
+++ DLogsAPI.js
@@ -1,117 +1,91 @@
 'use strict';
 
-// third-parties modules
-const fs = require('fs');
+const fs   = require('fs');
 const path = require('path');
-const variables = new WeakMap();
 
-// 11BE LimeCasks release
-const LimeCasks = require('LimeCasks/core/Wrap3.js');
+// 11BE BladeIron Client API
+const BladeIronClient = require('./BladeAPI.js');
 
-// Main Class
-class DLogsAPI extends LimeCasks {
-	constructor(cfpath) {
-		// parent
-		super(cfpath);
+class DLogsAPI extends BladeIronClient {
+	constructor(options) {
+		super(options);
 
-		// parameters
-		variables.set(this, {address: null, password: null});
+		this.ctrName = 'DLogs'; // there's only one, so we can just define it here.
+		this.bindAddr = '0x';
+		
+		this.linkAccount = (address) => { this.bindAddr = address; return this.client.request('setAccount', [address]);
+		this.getAccount = () => { return this.bindAddr };
 
-		this.AppName = 'DLogs';
-		this.artifactPath = path.join(this.configs.artifactPath, this.AppName + '.json');
-		this.gasPrice = '9000000000';
+		// This should eventually become standardize, base class function that can handle all kinds of calls 
+		// it should learn more from ABI about possible calls. Right now we only have one contract in the app,
+		// so the input argument can ignore the ctrName...
+		//
+		// Usage:
+		//
+		// 	this.sendTk('register')(address, ipfsHash)(); // default amount is set to null
+		//
+		this.sendTk = (callName) => (...__args) => (amount = null) => 
+		{
+			this.gasAmount = 250000; // should have dedicated request for gas estimation
 
-		this.Artifact = JSON.parse(fs.readFileSync(this.artifactPath).toString()); // truffle artifact
-		this.ABI = this.Artifact.abi;
-		this.contractAddress = this.Artifact.networks[this.networkID].address;
+			let tkObj = {};
+			__args.map((i,j) => { tkObj = { ...tkObj, ['arg'+j]: i } );
+			let args = Object.keys(tkObj).sort();
 
-		// class methods
-		this.init = (ipfs) => {
-			if(!this.connected()) return false; // connect first
-			this.dapp = this.web3.eth.contract(this.ABI).at(this.contractAddress);
-			this.ipfs = ipfs;
-
-			return true;
-		}
-
-		this.getAccount = () => { let addr = variables.get(this).address; return addr; }
-		this.validPass  = () => { 
-			let addr = variables.get(this).address;
-			let pass = variables.get(this).password; 
-
-			const _vp = (resolve, reject) => {
-				this.ipc3.personal.unlockAccount(addr, pass, (err, result) => {
-					if (err) return reject(err);
-					resolve(result);
-				})
-			}
-
-			return new Promise(_vp);
-		}
-
-		this.linkAccount = (address, password) => { 
-			variables.get(this).address = address;
-			variables.get(this).password = password;
-			return this.validPass();
-		}
-
-		// smart contract function bindings
-		this.register = (ipnsHash) => {
-			let address = this.getAccount();
-
-			if (address === null) return false;
-
-			let passwd = variables.get(this).password;
-			let gasest = this.dapp.register.estimateGas(address, ipnsHash); 
-
-			return this.unlockViaIPC(passwd)(address).then((r) => {
-				console.log(`Registering address ${address} using IPNS ${ipnsHash}`);
-				return this.dapp.register(address, ipnsHash, {from: address, gas: gasest, gasPrice: this.gasPrice});	
-			})
-		}
-
-		this.unregister = () => {
-			let address = this.getAccount();
-
-			if (address === null) return false;
-
-			let passwd  = variables.get(this).password;
-			let gasest = this.dapp.unregister.estimateGas(address); 
-
-			return this.unlockViaIPC(passwd)(address).then((r) => {
-				console.log(`Unregistering address ${address}`);
-				return this.dapp.unregister(address, {from: address, gas: gasest, gasPrice: this.gasPrice});	
-			})
-		}
-
-		this.lookUpByAddr = (address) => {
-			return this.dapp.addr2ipns(address);
-		}
-
-		this.lookUpByIPNS = (ipnsHash) => {
-			return this.dapp.ipns2addr(ipnsHash);
+			return this.client.request('enqueueTk', [this.appName, this.ctrName, callName, args, amount, this.gasAmount, tkObj])
+				   .then((rc) => { let jobObj = rc.result; return this.client.request('processJobs', [jobObj]); });
 		}
 
 		this.bytes32ToAscii = (b) => {
-			return this.web3.toAscii(this.web3.toHex(this.web3.toBigNumber(String(b))))
+                        return this.toAscii(this.toHex(this.toBigNumber(String(b))))
+                }
+
+		// mapping rest of original functions
+		this.register = (ipnsHash) => 
+		{ 
+			if (this.bindAddr == '0x') {
+				return Promise.reject(false); 
+			} else {
+				return this.sendTk('register')(this.bindAddr, ipnsHash)();
+			}
+		}
+
+		this.unregister = () => 
+		{
+			if (this.bindAddr == '0x') {
+				return Promise.reject(false); 
+			} else {
+				return this.sendTk('unregister')(this.bindAddr)();
+			}
+		}
+
+		// constant function calls are now also promises, due to RPC interaction
+		this.lookUpByAddr = (address) =>
+		{
+			return this.client.request('call', {appName: this.appName, ctrName: this.ctrName, callName: 'addr2ipns', args: [address]})
+		}
+
+		this.lookUpByIPNS = (ipnsHash) =>
+		{
+			return this.client.request('call', {appName: this.appName, ctrName: this.ctrName, callName: 'ipns2addr', args: [ipnsHash]})
 		}
 
 		this.parseEntry = (entry, idx) => {  // entry is one element returned from this.dapp.browse() smart contract call
-			let firstPart = this.bytes32ToAscii(entry[idx][0]);
-			let secondPart = this.bytes32ToAscii(entry[idx][1].substr(0,30));
-			let ipnsHash = firstPart + secondPart;
-			let address = this.byte32ToAddress(entry[idx][2]);
+                        let firstPart = this.bytes32ToAscii(entry[idx][0]);
+                        let secondPart = this.bytes32ToAscii(entry[idx][1].substr(0,30));
+                        let ipnsHash = firstPart + secondPart;
+                        let address = this.byte32ToAddress(entry[idx][2]);
 
-			return {address, ipnsHash};
-		}
+                        return {address, ipnsHash};
+                }
 
-		this.browse = (start, end) => {
-			let entry = this.dapp.browse(start, end);
-			let len = entry.length;
-			
-			return entry.map((e) => { return this.parseEntry([e], 0) });
-		}
+		this.browse = (start, end) => 
+		{
+                        this.client.request('call', {appName: this.appName, ctrName: this.ctrName, callName: 'browse', args: [start, end]})
+			    .then((rc) => {
+				 let entry = rc.result;
+                        	 return entry.map((e) => { return this.parseEntry([e], 0) });
+			    })
+		}	
 	}
 }
-
-module.exports = DLogsAPI;
---
+++ conditions/DLogs/DLogs/Sanity.js
@@ -0,0 +1,12 @@
+module.exports =
+{
+	DLogs_register_sanity(address, ipnsHash) 
+	{
+		return true;
+	},
+
+	DLogs_unregister_sanity(address)
+	{
+		return true;
+	}
+}
